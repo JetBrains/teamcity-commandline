952fcb6b3deaf46e94e4e4239cad7a877e781dd9
Konstantin.Donskov <Konstantin.Donskov>
2009-08-26 16:05:56 +0000

    #cleanup

--- command.line/src/com/jetbrains/teamcity/vcs/IVCSCommand.java
+++
@@ -1,9 +0,0 @@
-package com.jetbrains.teamcity.vcs;
-
-import java.util.Collection;
-
-public interface IVCSCommand {
-	
-	public Collection<String> execute(final String root, final CommandLineArguments arguments) throws ETeamException;
-
-}
