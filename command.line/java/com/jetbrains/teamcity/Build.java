844160963d08d765e98fd67ef26051db8a8c6ed5
kir <kir@jetbrains.com>
2013-01-25 13:48:47 +0000

    Fix command-line builder

--- command.line/java/com/jetbrains/teamcity/Build.java
+++ command.line/java/com/jetbrains/teamcity/Build.java
@@ -1,6 +1,6 @@
 package com.jetbrains.teamcity;
 
 public class Build {
-    public static final String major = "5.0 EAP";
+    public static final String major = "8.0";
     public static final String build = "SNAPSHOT";
 }
