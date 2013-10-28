376fe65923a1dc5480099a856766053fabe9e577
Maxim.Manuylov <Maxim.Manuylov@jetbrains.com>
2013-10-28 21:58:06 +0000

    fix "Unused import" inspections

--- command.line.test/src/com/jetbrains/teamcity/command/CommandRegistryTest.java
+++ command.line.test/src/com/jetbrains/teamcity/command/CommandRegistryTest.java
@@ -1,8 +1,6 @@
 package com.jetbrains.teamcity.command;
 
-import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertFalse;
-import static org.junit.Assert.assertNotNull;
 import static org.junit.Assert.assertTrue;
 
 import org.junit.Test;
